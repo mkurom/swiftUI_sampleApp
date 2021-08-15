45bca8bb833475e2cea74ca11c175b4e45a78ac6
yasudamakoto <hare.v1v1enne@gmail.com>
2021-08-15 17:56:12 +0900

    make janken App

---
+++ jankenApp/jankenApp/jankenAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  jankenAppApp.swift
+//  jankenApp
+//
+//  Created by 安田誠 on 2021/07/12.
+//
+
+import SwiftUI
+
+@main
+struct jankenAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
