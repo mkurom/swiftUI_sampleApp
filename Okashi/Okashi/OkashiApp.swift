e5a866edb46e6f807793ea582c5a5b7f64f4c14c
yasudamakoto <hare.v1v1enne@gmail.com>
2023-03-18 18:16:01 +0900

    first commit

---
+++ Okashi/Okashi/OkashiApp.swift
@@ -0,0 +1,17 @@
+//
+//  OkashiApp.swift
+//  Okashi
+//
+//  Created by 安田誠 on 2023/03/18.
+//
+
+import SwiftUI
+
+@main
+struct OkashiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Okashi/Okashi/OkashiData.swift
@@ -0,0 +1,8 @@
+//
+//  OkashiData.swift
+//  Okashi
+//
+//  Created by 安田誠 on 2023/03/18.
+//
+
+import Foundation
